59f559efa68eec9b42572bb9ebeabafe9fe59ba2
binaryfs <github@myrk.de>
2020-03-12 23:51:35 +0100

    Remove unneeded params from unit tests

--- demo/subfolder/Enum-test.lua
+++ demo/subfolder/Enum-test.lua
@@ -9,19 +9,19 @@ local Enum = require "demo.subfolder.Enum"
 local test = lovecase.newTestSet("Enum")
 
 test:group("new()", function(test)
-  test:run("should create an enum from a sequence", function(test)
+  test:run("should create an enum from a sequence", function()
     local Color = Enum.new{"Red", "Green", "Blue"}
     test:assertEqual(#Color, 3)
     test:assertEqual(Color.Red, 1)
     test:assertEqual(Color.Green, 2)
     test:assertEqual(Color.Blue, 3)
   end)
-  test:run("should create an enum from a hash map", function(test)
+  test:run("should create an enum from a hash map", function()
     local Direction = Enum.new{Left = "left", Right = "right"}
     test:assertEqual(Direction.Left, "left")
     test:assertEqual(Direction.Right, "right")
   end)
-  test:run("should create an enum from mixed input", function(test)
+  test:run("should create an enum from mixed input", function()
     local Color = Enum.new{"Red", "Green", Blue = "blue"}
     test:assertEqual(Color.Red, 1)
     test:assertEqual(Color.Green, 2)
@@ -31,35 +31,33 @@ end)
 
 test:group("caseOf()", function(test)
   local Color = Enum.new{"Red", "Green", Blue = "blue"}
-  test:run("should return the case of the given value", function(test)
+  test:run("should return the case of the given value", function()
     test:assertEqual(Color:caseOf(1), "Red")
     test:assertEqual(Color:caseOf(2), "Green")
     test:assertEqual(Color:caseOf("blue"), "Blue")
   end)
-  test:run("should return nil for unknown values", function(test)
+  test:run("should return nil for unknown values", function()
     test:assertEqual(Color:caseOf("green"), nil)
   end)
 end)
 
 test:group("verify()", function(test)
   local Direction = Enum.new{"Left", "Right"}
-  test:run("should accept valid values", function(test)
+  test:run("should accept valid values", function()
     test:assertEqual(Direction:verify(1), 1)
     test:assertEqual(Direction:verify(2), 2)
   end)
-  test:run("should raise an error for unknown values", function(test)
+  test:run("should raise an error for unknown values", function()
     test:assertError(function()
       Direction:verify(7)
     end)
   end)
 end)
 
-test:group("__newindex()", function(test)
+test:run("should not be extensible", function(test)
   local Direction = Enum.new{"Left", "Right"}
-  test:run("should not allow adding values", function(test)
-    test:assertError(function()
-      Direction.Down = 3
-    end)
+  test:assertError(function()
+    Direction.Down = 3
   end)
 end)
 
