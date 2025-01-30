0ef425762adc332c918ae25a3f96b328b6d20b73
Fabian Staacke <github@myrk.de>
2025-01-30 23:08:34 +0100

    Update LÖVE demo

--- conf.lua
+++ conf.lua
@@ -1,6 +1,7 @@
--- LÖVE configuration file.
+-- LÖVE demo configuration file.
 
 function love.conf(t)
   t.window.title = "LÖVE lovecase demo"
+  t.modules.window = false
   t.console = true
 end
--- main.lua
+++ main.lua
@@ -4,9 +4,6 @@ local lovecase = require("init")
 
 function love.load()
   local report = lovecase.runAllTestFiles("tests", true)
-  print(report:printResults())
-end
-
-function love.draw()
-  love.graphics.print("The lovecase results are output in the terminal.", 200, 300)
+  print(report:getResults())
+  love.event.quit()
 end
